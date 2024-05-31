e1043ad1433bc4736309c9a3265a70ea6696ee53
ChinnaBabuVanarasi <chinnababu.vanarasi@gmail.com>
2024-06-01 03:04:56 +0530

    Added database_connection.py where we have functions which are used to connect to db and get collections

---
+++ src/utilities/database_connection.py
@@ -0,0 +1,40 @@
+import json
+import os
+
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+load_dotenv()
+
+COLLECTIONS = {
+    "get_manga_links": "MANGALINKS",
+    "get_manga_images": "MANGAIMAGES",
+    "get_manga_chapters": "CHAPTERSDB",
+    "get_manga_details": "MANGADETAILS",
+    "get_csv_links": "CSVLINKS",
+}
+
+
+def get_database_connection():
+    """Creates a connection to the MongoDB database."""
+    try:
+        password = os.getenv("MONGODB_PASSWORD")
+        username = os.getenv("MONGODB_CLUSTER")
+        dbname = os.getenv("MONGODB_DATABASE")
+        uri = f"mongodb+srv://mongodb:{password}@{username}.gwrvbi6.mongodb.net/?retryWrites=true&w=majority"
+        return MongoClient(uri)[dbname]
+    except Exception as e:
+        print(f"Failed to connect to MongoDB: {e}")
+        return None
+
+
+def get_collection(collection_name: str, db=None):
+    """Fetches a specific collection from the MongoDB database."""
+    if db is None:  # Create connection if not provided
+        db = get_database_connection()
+    collection = os.getenv(COLLECTIONS.get(collection_name))
+    print(collection)
+    if collection:
+        return db[collection]
+    raise ValueError(
+        f"No Collection Found with the name '{collection_name}'. Please check your collection name and try again.")
