06f8d53f1d449e85418b99b0836cbdad767b3e9d
ChinnaBabuVanarasi <chinnababu.vanarasi@gmail.com>
2024-07-31 20:15:29 +0530

    updated logger_setup.py script

--- src/utilities/logger_setup.py
+++ src/utilities/logger_setup.py
@@ -1,34 +1,50 @@
 import logging
 import os
 from datetime import datetime
+from logging import getLogger, FileHandler, Formatter
 from pathlib import Path
 
 
-def setup_logging(filename):
-    if "chapter" in filename:
-        filename = f"Chapters/{filename}"
-        name = filename.split("/")[1].split("_logger")[0]
-    else:
-        filename = filename
-        name = filename.split("_logger")[0]
+def setup_logging(filename, log_level=logging.INFO, default_log_dir="data_ingestion_logs"):
+    """
+    Sets up a logger with a dedicated log file and folder based on the provided filename.
+
+    Args:
+        filename (str): The name of the logger and the base name of the log file.
+                           May optionally contain a subdirectory structure separated by '/'.
+        log_level (int, optional): The logging level (e.g., logging.DEBUG, logging.INFO).
+                                   Defaults to logging.INFO.
+        default_log_dir (str, optional): The default directory to create data_ingestion_logs in.
+                                          Defaults to "data_ingestion_logs" within the parent directory of the current working directory.
+
+    Returns:
+        logging.Logger: The created logger instance.
+    """
+    # Determine log directory based on filename structure and user preference
     try:
-        log_directory = os.path.join(Path(os.getcwd()).parent.parent.resolve(), 'logs')
+        # Up two levels from current working directory
+        log_directory = os.path.join(Path(os.getcwd()).resolve().parent.parent, default_log_dir)
+
     except FileNotFoundError:
-        log_directory = r"C:\\Users\\chinn\\PycharmProjects\\MangaTrackerX\\logs"
-    log_file_name = str(os.path.join(f"{log_directory}/{filename}",
-                                     f"{datetime.now().strftime('%Y%m%d-%H%M%S')}.log"))
+        log_directory = r"D:\PythonProjects\MangaTrackerX\data_ingestion_logs"
+
+    log_file_name = os.path.join(
+        f"{log_directory}/{filename}",
+        f"run_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log",
+    )
+    print(log_file_name)
+
+    # Create logger directory if it doesn't exist, ignoring potential errors if it already does
     os.makedirs(os.path.dirname(log_file_name), exist_ok=True)
+    #
+    # Create the logger instance
+    logger = getLogger(filename)
+    logger.setLevel(log_level)
 
-    # Create a logger instance
-    logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)
-
-    # Create a file handler
-    file_handler = logging.FileHandler(log_file_name)
-    file_handler.setLevel(logging.INFO)
-
-    # Create a log format
-    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    # Create a file handler with the specified log level and formatter
+    file_handler = FileHandler(log_file_name)
+    file_handler.setLevel(log_level)
+    formatter = Formatter("%(asctime)s - %(levelname)s - %(message)s")
     file_handler.setFormatter(formatter)
 
     # Add the file handler to the logger
