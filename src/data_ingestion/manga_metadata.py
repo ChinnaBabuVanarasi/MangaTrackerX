6b087edbc22e573fe3d8d9029e00baa6a186711b
ChinnaBabuVanarasi <chinnababu.vanarasi@gmail.com>
2024-07-31 20:34:50 +0530

    removed commented linesfrom code

--- src/data_ingestion/manga_metadata.py
+++ src/data_ingestion/manga_metadata.py
@@ -1,3 +1,5 @@
+import logging
+
 from src.utilities.database_connection import get_collection
 from src.utilities.insert_links_metadata import process_and_insert_manga_data
 from src.utilities.read_manga_links_csv_db import get_links
@@ -8,7 +10,6 @@ def insert_links_to_db(manga_links=None):
     """Inserts manga links into the database, either from CSV or DB."""
     collection_name = get_collection("get_manga_metadata")
     link_data = [link['manga_url'] for link in manga_links]
-    # link_data=['https://mangatx.to/manhua/gods-gambit/']
     log_name = setup_logging(filename="manga_metadata_insert")
     process_and_insert_manga_data(links_list=link_data, collection_name=collection_name, logger=log_name)
 
