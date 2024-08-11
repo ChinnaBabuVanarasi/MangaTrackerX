13183d901106b0d120d653b5fec4117f07972edb
ChinnaBabuVanarasi <chinnababu.vanarasi@gmail.com>
2024-08-11 20:04:00 +0530

    removed all endpoints and made only for chapters n chpater

--- src/api/app.py
+++ src/api/app.py
@@ -1,26 +1,27 @@
+from urllib.parse import unquote
+
 from fastapi import FastAPI
 from fastapi import FastAPI
 
-from src.utilities.mongodb_crud import get_records
+from src.utilities.mongodb_crud_api import get_records
 
 app = FastAPI()
 
 
 @app.get('/api/links')
 async def get_all_links():
-    records = get_records('get_manga_chapters')
+    records = get_records(endpoint='all_links')
     links = [record['manga_url'] for record in records]
     return links
 
 
-@app.get('/api/metadata')
+@app.get('/api/chapters')
 async def get_all_metadata():
-    records = get_records('get_manga_chapters')
-    metadata = [{k: v for k, v in d.items() if k != 'latest_chapters'} for d in records]
-    return metadata
+    records = get_records(endpoint='chapters')
+    return records
 
 
-@app.get('/api/metadata/{manga_title}')
-async def get_metadata_one(manga_title: str):
-    records = get_records(collection_name='get_manga_chapters', search_key=manga_title)
+@app.get('/api/chapter')
+async def get_metadata_one(query: str):
+    records = get_records(search_key=query, endpoint='chapter')
     return records
