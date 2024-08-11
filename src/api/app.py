3cbda2cb4575318ab568d1d7a9b7a5a0dda01265
ChinnaBabuVanarasi <chinnababu.vanarasi@gmail.com>
2024-08-11 18:47:37 +0530

    added endpoints

--- src/api/app.py
+++ src/api/app.py
@@ -1,4 +1,26 @@
 from fastapi import FastAPI
+from fastapi import FastAPI
+
+from src.utilities.mongodb_crud import get_records
 
 app = FastAPI()
 
+
+@app.get('/api/links')
+async def get_all_links():
+    records = get_records('get_manga_chapters')
+    links = [record['manga_url'] for record in records]
+    return links
+
+
+@app.get('/api/metadata')
+async def get_all_metadata():
+    records = get_records('get_manga_chapters')
+    metadata = [{k: v for k, v in d.items() if k != 'latest_chapters'} for d in records]
+    return metadata
+
+
+@app.get('/api/metadata/{manga_title}')
+async def get_metadata_one(manga_title: str):
+    records = get_records(collection_name='get_manga_chapters', search_key=manga_title)
+    return records
