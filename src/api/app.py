d28ae51e0fa326dd1f183508062423b5aa1e14ae
ChinnaBabuVanarasi <chinnababu.vanarasi@gmail.com>
2024-09-18 15:15:42 +0530

    updated app with post method

--- src/api/app.py
+++ src/api/app.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get('/')
 async def get_all_links():
     return "Hey there! Welcome to MangatrackerX"
+
+
+@app.post('/api/manga_link')
+async def add_manga_link(manga_url: str):
+    return manga_url
